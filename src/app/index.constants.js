4c1129ba8539e6e5e3cc67f366b06aaf050cb1d0
Brian Staruk <bstaruk@gmail.com>
2015-10-19 10:31:52 -0400

    initial commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('brianWebAngular')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
