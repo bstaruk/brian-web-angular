c975ea49dcc80e28287a70bfadc96186624b2cdc
Brian Staruk <bstaruk@gmail.com>
2016-02-25 12:52:59 -0500

    a little angular magic

--- src/app/pages/main/main.controller.js
+++ src/app/pages/main/main.controller.js
@@ -1,12 +1,42 @@
 (function () {
-	'use strict';
+  'use strict';
 
-	angular
-		.module('brianWebAngular')
-		.controller('MainController', MainController);
+  angular
+    .module('brianWebAngular')
+    .controller('MainController', function () {
+      var vm = this;
+      vm.skills = [
+        {
+          id: 1,
+          title: 'HTML, CSS & JS',
+          shown: false
+        },
+        {
+          id: 2,
+          title: 'jQuery',
+          shown: false
+        },
+        {
+          id: 3,
+          title: 'AngularJS',
+          shown: false
+        },
+        {
+          id: 4,
+          title: 'ReactJS',
+          shown: false
+        },
+        {
+          id: 5,
+          title: 'PHP',
+          shown: false
+        },
+        {
+          id: 6,
+          title: 'LAMP System Ops',
+          shown: false
+        }
+      ];
+    });
 
-	/** @ngInject */
-	function MainController() {
-		// stuff goes here
-	}
 })();
