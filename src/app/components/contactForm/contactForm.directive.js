3a699958c2a4abd278b5f5497fa70f689c21fce5
Brian Staruk <bstaruk@gmail.com>
2016-02-26 09:35:28 -0500

    add dummy sendmail

--- src/app/components/contactForm/contactForm.directive.js
+++ src/app/components/contactForm/contactForm.directive.js
@@ -6,15 +6,44 @@
       return {
         restrict: 'E',
         templateUrl: 'app/components/contactForm/contactForm.html',
-        controller: function($scope) {
+        controller: function($scope, $http, $window) {
+
           $scope.errors = {};
           $scope.submitted = false;
+
+          $scope.sendMail = function() {
+            var apiURL = 'https://api.sendgrid.com/api/mail.send.json',
+            mailJSON ={
+              'api_user': 'bstaruk',
+              'api_key': 'w-RTWPlyQGmzZDoaBwwEww',
+              'to[]': 'bstaruk@gmail.com',
+              'toname[]': 'Brian Staruk',
+              'subject': 'Contact Form Submission - brian.staruk.me',
+              'text': 'this is a test email',
+              'from': 'noreply@staruk.me'
+            };
+            $http.post(apiURL, mailJSON).
+            success(function(data, status, headers, config) {
+              $window.console.log('successful email send.');
+              $window.console.log('status: ' + status);
+              $window.console.log('data: ' + data);
+              $window.console.log('headers: ' + headers);
+              $window.console.log('config: ' + config);
+            }).error(function(data, status) {
+              $window.console.log('error sending email.');
+              $window.console.log('status: ' + status);
+            });
+            $window.console.log(mailJSON);
+          };
+
           $scope.notSubmitted = function() {
             $scope.submitted = false;
           };
+
           $scope.handleForm = function(form) {
             $scope.submitted = true;
-            window.console.log(form);
+            $window.console.log(form);
+            $scope.sendMail();
           };
         }
       };
