64d6714dbcb26cc880dc967527684f56c0baf624
Brian Staruk <bstaruk@gmail.com>
2016-02-29 16:49:59 -0500

    fix fonts build task

--- gulp/build.js
+++ gulp/build.js
@@ -71,10 +71,9 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-  return gulp.src($.mainBowerFiles())
-    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
-    .pipe($.flatten())
-    .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+	return gulp.src('bower_components/**/*.{eot,svg,ttf,woff,woff2}')
+		.pipe($.flatten())
+		.pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
 });
 
 gulp.task('other', function () {
